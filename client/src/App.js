79ecf7246b11ee473e421ef318b2557e7594ca56
Narendras <n.syahrasyad.n@gmail.com>
2019-01-22 00:30:53 -0800

    Integrated prettier and eslint

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,7 @@
-import React, { Component } from "react";
+import React from 'react';
+import './App.css';
+import Dashboard from './components/Dashboard/Dashboard';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = props => <Dashboard />;
 
 export default App;
