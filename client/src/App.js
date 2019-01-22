3c2dcb60c5309302878e9c3f3d6b58d49f906c81
Narendras <n.syahrasyad.n@gmail.com>
2019-01-22 00:59:05 -0800

    Bootstrapped simple dashboard page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard/Dashboard';
 
-const App = props => <Dashboard />;
+class App extends Component {
+  state = {
+    funds: 500,
+    goal: 1500,
+    budget: {
+      total: 1000,
+      categories: [
+        {
+          name: 'Food',
+          amount: 600,
+          spent: 200
+        },
+        {
+          name: 'Shopping',
+          amount: 400,
+          spent: 50
+        }
+      ]
+    }
+  };
+
+  render() {
+    const { funds, goal, budget } = this.state;
+    return <Dashboard funds={funds} goal={goal} budget={budget} />;
+  }
+}
 
 export default App;
--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,8 +1,41 @@
 import React, { Component } from 'react';
 
 class Dashboard extends Component {
+  state = {
+    funds: 0,
+    goal: 0,
+    budgetTotal: 0,
+    categories: [],
+    expenseTotal: 0
+  };
+
+  static getDerivedStateFromProps({ funds, goal, budget }) {
+    const { total, categories } = budget;
+    const budgetTotal = total;
+    const expenseTotal = categories.reduce((acc, curr) => acc + curr.spent, 0);
+
+    return { funds, goal, budgetTotal, categories, expenseTotal };
+  }
+
   render() {
-    return <h1>Hello World!</h1>;
+    const { funds, goal, budgetTotal, categories, expenseTotal } = this.state;
+    return (
+      <div>
+        <h1>{goal - funds}</h1>
+        <p>Left until you reach your goal!</p>
+        <p>{budgetTotal - expenseTotal} remaining in budget.</p>
+        {categories.map(ctg => {
+          const { name, amount, spent } = ctg;
+          return (
+            <div>
+              <p>{(amount / budgetTotal) * 100}%</p>
+              <p>{name}</p>
+              <p>{spent} spent</p>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
 
