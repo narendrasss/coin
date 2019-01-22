fa864ac0d3496a976dffe9a6bb7aba430548350b
Narendras <n.syahrasyad.n@gmail.com>
2019-01-22 01:41:49 -0800

    Added styling templates

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import './Dashboard.scss';
 
 class Dashboard extends Component {
   state = {
     funds: 0,
     goal: 0,
     budgetTotal: 0,
-    categories: [],
-    expenseTotal: 0
+    expenseTotal: 0,
+    categories: []
   };
 
   static getDerivedStateFromProps({ funds, goal, budget }) {
@@ -20,20 +21,26 @@ class Dashboard extends Component {
   render() {
     const { funds, goal, budgetTotal, categories, expenseTotal } = this.state;
     return (
-      <div>
-        <h1>{goal - funds}</h1>
-        <p>Left until you reach your goal!</p>
-        <p>{budgetTotal - expenseTotal} remaining in budget.</p>
-        {categories.map(ctg => {
-          const { name, amount, spent } = ctg;
-          return (
-            <div>
-              <p>{(amount / budgetTotal) * 100}%</p>
-              <p>{name}</p>
-              <p>{spent} spent</p>
-            </div>
-          );
-        })}
+      <div className="Dashboard">
+        <header className="Dashboard__goal">
+          <h1>${goal - funds}</h1>
+          <p>Left until you reach your goal!</p>
+        </header>
+        <div className="Dashboard__info">
+          <p className="opaque">{new Date().toDateString()}</p>
+          <p>{budgetTotal - expenseTotal} remaining in budget.</p>
+          <div className="Dashboard__graph" />
+          {categories.map(ctg => {
+            const { name, amount, spent } = ctg;
+            return (
+              <div className="Dashboard__category">
+                <p>{(amount / budgetTotal) * 100}%</p>
+                <p>{name}</p>
+                <p>${spent} spent</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
